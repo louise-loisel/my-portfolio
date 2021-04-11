1c12fb8ebf7eba837a8275c4e96afca05fb6b4db
Louise Loisel <loisel.louise@gmail.com>
2021-04-11 16:59:53 +0200

    Initialize

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './img/welcome.png';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello
+        </p>
+    </div>
+  );
+}
+
+export default App;
